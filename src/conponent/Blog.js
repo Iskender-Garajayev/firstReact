10efcbb05a439ae6a3abb2c6413b79202ab4799b
I-s-k-a-n-d-e-r <yoloten.isa@gmail.com>
2022-04-25 21:13:31 +0300

    first commit

---
+++ src/conponent/Blog.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Blog extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Where are you frolm ?</h2>
+      </div>
+    )
+  }
+}
